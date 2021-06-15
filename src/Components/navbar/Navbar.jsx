edc20e1a8aa780d0fd7c8c6801f32bf949c694aa
Randy Davoh <randyelorm@yahoo.com>
2021-06-15 18:50:45 +0000

    worked on nav mobile display

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -6,7 +6,16 @@ const Navbar = () => {
         <div>
               
                 <nav class="navbar navbar-default navbar-home">
-                    <a class="navbar-brand" href="#">PIZZA MANNIA </a>
+                <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">PIZZAMANIA</a>
+                        </div>
+
                     
                 
                     <div class="collapse navbar-collapse my_nav_links" id="bs-example-navbar-collapse-1">
